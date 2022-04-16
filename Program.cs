38631e86e7a147cbb70a99ad5b345e42ae44fc0b
flomanuel <flomanuel.github@gmail.com>
2022-04-16 17:36:42 +0200

    cleanup, comments 	modified:   Program.cs 	modified:   obj/project.assets.json 	modified:   obj/project.nuget.cache 	modified:   obj/which-day.csproj.nuget.dgspec.json

--- Program.cs
+++ Program.cs
@@ -1,22 +1,55 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Text.RegularExpressions;
 
 public class Program {
 	public static void Main() {
+		GameLoop();
+	}
+
+	public static void GameLoop(){
 		Console.WriteLine("===== Which Date =====");
 
+		// Flag für die Programmschleife. Das Programm läuft, solange der Variablenwert true bleibt.
+		bool programLoop = true;
+
+		// Programmschleife
+		while(programLoop) {
+			Console.Write("Geben Sie bitte das Datum im Format 'DD - MM - YYYY' ein: ");
+			string stringDate = Console.ReadLine();
+			// Eingabe auf Format 'dd-mm-yyyy' oder 'dd - mm - yyyy' überprüfen.
+			if(ValidateString(stringDate)) {
+				// Wochentag berechnen
+				Console.WriteLine(
+					$"Der {stringDate} ist ein {CalculateDate(stringDate)}.");
+			} else {
+				// andernfalls: Fehlermeldung ausgeben
+				Console.WriteLine("Geben Sie das Datum bitte im korrekten Format ein!");
+			}
+			// Erfragen, ob das Programm beendet werden soll.
+			Console.Write("Möchten Sie ein weiteres Datum ermitteln lassen? J / N: ");
+			programLoop = Console.ReadLine().ToLower() == "j"? true : false;
+		}
+		Console.WriteLine("Bis bald.");
+	}
+
+	public static bool ValidateString(string candidate){
+		return new Regex("^\\d{1,2}(-| - )\\d{1,2}(-| - )\\d{1,4}$").IsMatch(candidate);
+	}
+
+	public static string CalculateDate(string stringDate) {
 		// Tagesdatum
 		int day;
 
-		string[] weekdayMapping = new string[7] {"Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag"};
+		List<string> weekdayMapping = new List<string> {"Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag"};
 
-		/**
-		 * Monat, julianischer Kalender
-		 * Format: März = 1 | April = 2 | Mai = 3 | Juni = 4 | Juli = 5 | August = 6 | September = 7 | Oktober = 8 | November = 9 | Dezember = 10 | Januar = 11 | Februar = 12
-		 **/
 		int month;
 
-		int[] monthMapping = new int[] { 0, 11, 12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+		/**
+		 * Monate, julianischer Kalender
+		 * Format: März = 1 | April = 2 | Mai = 3 | Juni = 4 | Juli = 5 | August = 6 | September = 7 | Oktober = 8 | November = 9 | Dezember = 10 | Januar = 11 | Februar = 12
+		 **/
+		List<int> monthMapping = new List<int> {11, 12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
 		/**
 		 * Jahrzehnt
@@ -30,54 +63,42 @@ public class Program {
 		 **/
 		int century;
 
-		Console.Write("Geben Sie bitte das Datum im Format 'DD - MM - YYYY' ein: ");
-		string stringDate = Console.ReadLine();
 
-		// Eingabe auf Format 'dd-mm-yyyy' oder 'dd - mm - yyyy' überprüfen.
-		if(new Regex("^\\d{1,2}(-| - )\\d{1,2}(-| - )\\d{1,4}$").IsMatch(stringDate)) {
-			// Eingabe am Trennzeichen '-' aufteilen und whitespace entfernen.
-			string[] dateString = stringDate.Split('-');
+		string result = "";
+		// Eingabe am Trennzeichen '-' aufteilen und whitespace entfernen.
+		string[] dateString = stringDate.Split('-');
 
-			for(int i = 0; i < dateString.Length; ++i) {
-				dateString[i] = dateString[i].Trim();
-			}
-
-			// Tag formatieren
-			day = Convert.ToInt32(dateString[0]);
-
-			// Monat formatieren
-			month = monthMapping[Convert.ToInt32(dateString[1])];
-
-			// Jahrzehnt und Jahrhundert berechnen
-			int year = Convert.ToInt32(dateString[2]);
-
-			// Für Jan und Feb die Werte des Vorjahres verwenden.
-			if(month > 10 && year > 0) {
-				--year;
-			}
-
-			century = year/100;
-			decade = year%100;
-
-			Console.WriteLine(day);Console.WriteLine(month);Console.WriteLine(decade);Console.WriteLine(century);
-
-			/**
-			 * calculating day of week
-			 * https://de.wikipedia.org/wiki/Wochentagsberechnung#Formel
-			 **/
-			int weekday = Convert.ToInt32((day + Math.Floor(2.6*month - 0.2) + decade + Math.Floor(decade/4.0) + Math.Floor(century/4.0) - 2*century) % 7);
-			if(weekday < 0) {
-				weekday += 7;
-			}
-
-			Console.WriteLine("Der {0}.{1}.{2} ist ein {3}", dateString[0], dateString[1], dateString[2], weekdayMapping[weekday]);
-		} else {
-			Console.WriteLine("Die Anwendung wurde beendet.");
-			Console.WriteLine("Geben Sie das Datum bitte im korrekten Format ein!");
+		for(int i = 0; i < dateString.Length; ++i) {
+			dateString[i] = dateString[i].Trim();
 		}
 
+		// Tag formatieren
+		day = Convert.ToInt32(dateString[0]);
 
+		// Monat formatieren
+		month = monthMapping[Convert.ToInt32(dateString[1])-1];
+
+		// Jahrzehnt und Jahrhundert berechnen
+		int year = Convert.ToInt32(dateString[2]);
+
+		// Für Jan und Feb die Werte des Vorjahres verwenden.
+		if(month > 10 && year > 0) {
+			--year;
+		}
+
+		// Jahrhundert errechnen.
+		century = year/100;
+
+		// Jahrzehnt errechnen.
+		decade = year%100;
+
+		/**
+		 * Wochentag berechnen
+		 * https://de.wikipedia.org/wiki/Wochentagsberechnung#Formel
+		 **/
+		int weekday = Convert.ToInt32(
+				(day + Math.Floor(2.6*month - 0.2) + decade + Math.Floor(decade/4.0) + Math.Floor(century/4.0) - 2*century + 7 ) % 7
+			);
+		return weekdayMapping[weekday];
 	}
 }
-
-// todo: Fehler bei 01-01-1001, 12-1-1
